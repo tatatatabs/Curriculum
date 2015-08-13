5217121cd4bf263a58374f00fd3e9848929f1b22
Vincent St. Louis <vincentst.louis@gmail.com>
2015-08-13 17:33:57 -0400

    rename this as well

---
+++ Week_3/Day_2/Challenges/04_hard.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+    <p>
+
+        <?php
+            /*
+             * Find the valid hashtags in this string
+
+             Create a function that will find the 
+                - Hashtags are alphanumeric strings ([a-zA-Z0-9]) prefixed with the hash symbol "#" 
+                - (e.g. "#keyword" is a valid hashtag and #2Words!? counts as "2Words" only)
+                - Hashtags cannot be in the middle of a words or characters (e.g. "some#word" and ".?!#word" are invalid)
+                - Hash symbols ("#") alone are not valid
+                - If a word is preceded by more than one hash symbol, only one hashtag counts (e.g. "###keyword" counts only as "keyword")
+                - Since hashtags are only alphanumeric [a-zA-Z0-9] in case (e.g. "###keyword" counts only as "keyword")
+             *
+             */
+
+                function 
+
+             $sentence = 'this #free is ##wild , .?!#word #can #young some#word ###keyword';
+
+             $words = // find a php function that will turn a string into words
+
+             // now that you have words, loop around them to find the valid hashtags
+
+             // store the bad and good hashtags in their appropriate locations
+             $hashtags = array('bad_hashtags' => array(), 'good_hashtags' => array());
+             
+        ?>
+
+    </p>
+
+    </body>
+</html>
